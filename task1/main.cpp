3a8baaf7b97625409dc25be913fd146e546b9095
Balpoff <78598763+Balpoff@users.noreply.github.com>
2024-03-13 10:38:56 +0700

    Add files via upload

---
+++ task1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+constexpr long long mult = 10000000;
+
+#ifdef USE_DOUBLE
+typedef double DataType;
+#else
+typedef float DataType;
+#endif
+
+int main() {
+    std::vector<DataType> sins(mult);
+    DataType sum = 0;
+    for (long long i = 0; i < mult; ++i) {
+        sins[i] = std::sin(static_cast<DataType>(i) * 2 * M_PI / mult);
+        sum += sins[i];
+    }
+    std::cout << "Sum: " << sum << std::endl;
+    return 0;
+}
