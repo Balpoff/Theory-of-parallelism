a5b5d3d1c47629837295b2815408543345a05f42
Balpoff <78598763+Balpoff@users.noreply.github.com>
2024-06-04 05:00:34 +0700

    Update task.cpp

--- task6/task.cpp
+++ task6/task.cpp
@@ -9,17 +9,19 @@
 #include <nvtx3/nvToolsExt.h>
 #include "device_vector.h"
 
-#define OFFSET(x, y, m) (((x)*(m)) + (y)) // пластина должна быть натянута на сферу?
+#define OFFSET(x, y, m) (((x)*(m)) + (y)) // Макрос для вычисления смещения в матрице
 
+// Функция для инициализации теплового поля
 void initialize_field(device_vector<double>& matrix, std::vector<std::tuple<int, double>> heat_points) {
     for (auto heat_point : heat_points) {
-        int index = std::get<0>(heat_point);
-        double temp = std::get<1>(heat_point);
-        matrix[index] = temp;
+        int index = std::get<0>(heat_point); // Индекс точки нагрева
+        double temp = std::get<1>(heat_point); // Температура в этой точке
+        matrix[index] = temp; // Устанавливаем температуру в матрице
     }
-    matrix.update_device(0, matrix.size());
+    matrix.update_device(0, matrix.size()); // Обновляем данные на устройстве
 }
 
+// Функция для вывода теплового поля
 void draw_field(device_vector<double> matrix, int size) {
     for (int i = 1; i < size - 1; i++) {
         for (int j = 1; j < size - 1; j++)
@@ -28,12 +30,13 @@ void draw_field(device_vector<double> matrix, int size) {
     }
 }
 
+// Функция для вычисления одного шага теплового поля
 double calculate_step(device_vector<double>& matrix, device_vector<double>& matrix_out, int size){
     double err = 0;
     #pragma acc parallel loop reduction(max:err) collapse(2) async
     for (int i = 1; i < size - 1; i++) {
         for (int j = 1; j < size - 1; j++) {
-            double current_point = matrix[OFFSET(i, j, size)];
+            double current_point = matrix[OFFSET(i, j, size)]; // Текущая точка
             int num_of_points = 1;
             num_of_points += i > 1 ? 1 : 0;
             num_of_points += j > 1 ? 1 : 0;
@@ -45,7 +48,7 @@ double calculate_step(device_vector<double>& matrix, device_vector<double>& matr
                                              matrix[OFFSET(i + 1, j, size)] +
                                              matrix[OFFSET(i, j + 1, size)] +
                                              matrix[OFFSET(i, j, size)]) * (1 / (float)num_of_points); 
-            err = fmax(err, fabs(matrix_out[OFFSET(i, j, size)] - matrix[OFFSET(i, j, size)]));
+            err = fmax(err, fabs(matrix_out[OFFSET(i, j, size)] - matrix[OFFSET(i, j, size)])); // Вычисляем ошибку
         }
     }
     #pragma acc update self(matrix_out._A[:size * size]) async
@@ -53,6 +56,7 @@ double calculate_step(device_vector<double>& matrix, device_vector<double>& matr
     return err;
 }
 
+// Функция для копирования матрицы
 void copy_matrix(device_vector<double>& matrix, device_vector<double>& matrix_out, int size) {
     #pragma acc parallel loop
     for(int i = 0; i < size; i++) {
@@ -62,6 +66,7 @@ void copy_matrix(device_vector<double>& matrix, device_vector<double>& matrix_ou
     }
 }
 
+// Функция для вычисления теплового поля до заданной точности или максимального количества итераций
 void calculate_heatfield(device_vector<double>& matrix, device_vector<double>& matrix_out, int size, double max_error, int max_iterrations) {
     double error = 1;
     int it = 0;
@@ -84,15 +89,14 @@ void calculate_heatfield(device_vector<double>& matrix, device_vector<double>& m
 namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
-    //default values init
     int size = 10;
     double max_error = 1e-6;
     int max_iterations = 1000000;
 
-    // arg parser init
+    // Инициализация парсера аргументов командной строки
     po::options_description desc("Allowed options");
     desc.add_options()
-                    ("help,h", "Show help")//"-s - size\n-me - max error of calculation\n-mit - max iteration count of calculation\n")
+                    ("help,h", "Show help")
                     ("size,s", po::value<int>(&size), "field size")
                     ("max_error,me", po::value<double>(&max_error), "max error of calculation")
                     ("max_iterations,mit", po::value<int>(&max_iterations), "max iteration count of calculation")
@@ -105,25 +109,32 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-
+    // Создание и инициализация матриц
     device_vector<double> matrix = device_vector<double>((size+2) * (size+2));
-
     device_vector<double> matrix_out = device_vector<double>((size+2) * (size+2));
-    size += 2; // add padding
+    size += 2; // Добавление отступов
+
+    // Определение начальных точек нагрева
     std::vector<std::tuple<int, double>> heat_points({std::make_tuple(size + 1, 10), 
                                                     std::make_tuple(2 * (size) - 2 , 20), 
                                                     std::make_tuple(size * size - 2 * (size) + 1, 30), 
                                                     std::make_tuple(size * size - size - 2, 40)
                                                     });
+    // Инициализация теплового поля
     nvtxRangePushA("init");
     initialize_field(matrix, heat_points);
     nvtxRangePop();
+    
     std::cout << "size: " << size-2 << "x" << size-2 << '\n';
+
+    // Начало измерения времени
     const auto start{ std::chrono::steady_clock::now() };
     calculate_heatfield(matrix, matrix_out, size, max_error, max_iterations);
     const auto end{ std::chrono::steady_clock::now() };
     const std::chrono::duration<double> elapsed_seconds{ end - start };
     std::cout << elapsed_seconds.count() << " s\n";
+
+    // Если флаг вывода установлен, выводим конечную матрицу
     if (vm.count("draw_output")) {
         draw_field(matrix_out, size);
     }
